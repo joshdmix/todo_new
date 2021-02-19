e464d0a8dc85687e10649b7397b0372661f9146d
Josh Mix <josh.mix@blockfi.com>
2021-02-18 21:13:56 -0500

    dynamic queries appear to work
    '
    '

--- lib/todo/tasks.ex
+++ lib/todo/tasks.ex
@@ -37,106 +37,72 @@ defmodule Todo.Tasks do
   end
 
   def get_tasks(labels, priority, completed, sort_direction, sort_term) do
-    IO.inspect(label: "HERE")
-    IO.inspect(sort_direction, label: "sort direction")
-    IO.inspect(sort_term, label: "sort term")
+    priority  = get_tasks_by_priority(priority)
+    labels = get_tasks_by_labels(labels)
+    completed = get_completed_tasks(completed)
 
-    q1 = get_tasks_by_priority(priority)
+    query =
+      from t in Task,
+        where: ^priority,
+        where: ^labels,
+        where: ^completed,
+        order_by: [{^sort_direction, ^sort_term}]
 
-    q2 = get_tasks_by_labels(labels)
-
-    q3 = get_completed_tasks(completed)
-
-    IO.inspect(q1, label: "q1")
-    IO.inspect(q2, label: "q2")
-    IO.inspect(q3, label: "q3")
-
-    if priority == nil do
-      if completed == nil do
-        if labels == nil do
-          Repo.all(from t in Task, order_by: [{^sort_direction, ^sort_term}])
-        end
-      end
-    end
-
-    # IO.inspect(q3)
-
-    # Task |> where()
-    # order_by: [sort_direction: sort_term],
-    # [
-    #   sort_tasks(sort_direction, sort_term),
-    #   get_completed_tasks(completed),
-    #   get_tasks_by_priority(priority)
-    # ]
-    # |> IO.inspect(label: "HERE")
-    # |> Enum.filter(&(!is_nil(&1)))
-    # |> IO.inspect(label: "HERE")
-    # |> List.first
-    # |> Enum.join(" and ")
-    # |> IO.inspect(label: "HERE")
-    # |> Repo.all()
+    IO.inspect(query, label: "query!!!!!")
+    Repo.all(query) |> format_tasks()
   end
 
   def sort_tasks(sort_direction \\ :asc, sort_term \\ :start_date) do
     values = [{sort_direction, sort_term}]
-    # Repo.all(from t in Task, order_by: ^values) |> format_tasks()
     Repo.all(from t in Task, order_by: ^values)
   end
 
-  def get_completed_tasks(nil),
-    do: from(t in Task, where: t.completed == true or t.completed == false)
+  def get_completed_tasks(nil) do
+    dynamic([t], t.completed == true or t.completed == false)
+  end
 
   def get_completed_tasks(completed) do
-    IO.inspect(completed, label: "GET COMPLETED TASKS")
     case completed do
       "Completed" ->
-        from t in Task, where: t.completed == true
+        dynamic([t], t.completed == true)
 
       _ ->
-        from t in Task, where: t.completed != true
+        dynamic([t], t.completed != true)
     end
   end
 
-
-
-  # def get_completed_tasks(:true) do
-  #   Repo.all(from t in Task, where: t.completed == true)
-  # end
-
-  # def get_uncompleted_tasks(:false) do
-  #   Repo.all(from t in Task, where: t.completed == false)
-  # end
-
-  def get_tasks_by_labels(nil), do: from(t in Task, where: nil not in t.labels)
-
-  def get_tasks_by_labels(label) do
-    from t in Task, where: ^label in t.labels
-    # query = &Repo.all(from(t in Task, where: ^&1 in t.labels))
-    # labels |> Enum.map(query) |> List.flatten() |> Enum.uniq() |> format_tasks()
+  def get_tasks_by_labels(nil) do
+    dynamic([t], nil not in t.labels)
   end
 
-  def get_tasks_by_priority(nil), do: from(t in Task, where: not is_nil(t.priority))
+  def get_tasks_by_labels(label) do
+    dynamic([t], ^label in t.labels)
+  end
+
+  def get_tasks_by_priority(nil) do
+    dynamic([t], not is_nil(t.priority))
+  end
 
   def get_tasks_by_priority(priority) do
-    from t in Task, where: t.priority == ^priority
+    dynamic([t], t.priority == ^priority)
   end
 
   ###############
 
-  def get_shift_list(date, interval, repeat_qty, unit) do
-    repeat_list = 1..repeat_qty |> Enum.to_list()
-    build_shift_list(repeat_list, [], date, interval, unit)
-  end
+  # def get_shift_list(date, interval, repeat_qty, unit) do
+  #   repeat_list = 1..repeat_qty |> Enum.to_list()
+  #   build_shift_list(repeat_list, [], date, interval, unit)
+  # end
 
-  defp build_shift_list([], date_list, _date, _interval, _unit) do
-    date_list
-  end
+  # defp build_shift_list([], date_list, _date, _interval, _unit) do
+  #   date_list
+  # end
 
-  defp build_shift_list([_hd | tl], date_list, date, interval, unit) do
-    shift = [{unit, interval}]
-    shifted_date = date |> Timex.shift(shift)
-    build_shift_list(tl, [shifted_date | date_list], shifted_date, interval, unit)
-  end
+  # defp build_shift_list([_hd | tl], date_list, date, interval, unit) do
+  #   shift = [{unit, interval}]
+  #   shifted_date = date |> Timex.shift(shift)
+  #   build_shift_list(tl, [shifted_date | date_list], shifted_date, interval, unit)
+  # end
 
   def list_alphabetical_labels do
     Label |> Labels.alphabetical() |> Repo.all() |> Enum.map(& &1.name)
@@ -158,7 +124,8 @@ defmodule Todo.Tasks do
   end
 end
 
-# def create_tasks_on_interval(task, interval, repeat_qty, unit \\ :weeks) do
+#
+#def create_tasks_on_interval(task, interval, repeat_qty, unit \\ :weeks) do
 #   shifted_dates
 
 # end
